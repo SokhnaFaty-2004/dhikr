824f6bdaada6342fb5ad302d9edcda88bf1bf6ab
nexusDev <nexotech.dev@gmail.com>
2026-02-08 14:35:14 +0000

    first commit

---
+++ scriptMaxuzz.js
@@ -0,0 +1,116 @@
+
+      // Tableau des versets
+      const wird = [
+        {
+          arabe: "أَعُوذُ بِاللَّهِ مِنَ الشَّيْطَانِ الرَّجِيمِ",
+          phonetic: "Ahuuzu biallahi minash shayţānir rajiimi (1)",
+        },
+        {
+          arabe:
+            "وَإِنِّي أُعِيذُهَا بِكَ وَذُرِّيَّتَهَا مِنَ الشَّيْطَانِ الرَّجِيمِ",
+          phonetic:
+            "Wa Innī ya uhiizuhā bika wa zurrīyatahā mina-sh shayţāni-r rajīmi (1)",
+        },
+        {
+          arabe:
+            "رَبِّ أَعُوذُ بِكَ مِنْ هُمَزَاتِ الشَّيَاطِينِ وَأَعُوذُ بِكَ رَبِّ أَنْ يَحْضُرُونِ",
+          phonetic:
+            "Rabbi ahuuzu bika min heumazaatish shayaatiini, wa ahuuzu bika rabbi an yahduruun (1)",
+        },
+        {
+          arabe: "بِسْمِ اللّهِ الرَّحْمَنِ الرَّحِيمِ ",
+          phonetic:
+            "Bismil laahi-r Raĥmaani-r raĥiimi,  \n Allaa humma bihaqi wajhil laahi tahaalal kariimi, al’lazi azhabas salaasata was salaasiina ilaa ghayri saahibi haazal wirdil mubaaraki, wa'iilaa ghayri makhtiira lahu, bilaa tawjiihi shay’inne minhaa ilayhi, wa ilaa makhtiira lahu abadane, salli wa sallim wa baarik halaa sayyidinaa wa mawlaana muhamadine, wa aalihii wa sahbihii, wajhal haazal wirdal mubaaraka hisnane hasiinane hane jamiihi ahdaa’ika, wa sitrane maa’nihane min jamiihil mafaasidi, wa farajane li jamiihil muslimiina wal muslimaati, aamiin ya rab’al haalamiina(1) ",
+        },
+        {
+          arabe:
+            " َأََعََوَذَ َبَكََلََمََاَتَ اََللهََاََلََاَمََاَتَ َمََنَ َشَ َمَا َخََلََقَ َ",
+          phonetic:
+            "Ahuuzu bikalimaatil laahit-taam’maati min sharri maa khalakha (3)",
+        },
+        {
+          arabe:
+            "بِسْمِ َاَللهَََاَ َلَي َلَ َيََضَََمََعَ َاَسََمََهَََشََءَ ََفَ اََلََرََ ََض َوََلَ ََفَ َاَلَسََمََاَءَََوَََهَوَ اََلَسََمََيَعَ َاَلََعََلََيَمََ",
+          phonetic:
+            "Bismillâhil-lazii la yadurru mahas-mihii shay’une fil-ardi walaa fis-samaa’i wa huwas-samii hul haliimu. (3)",
+        },
+        {
+          arabe:
+            "١. بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ٢. ٱلْحَمْدُ لِلَّهِ رَبِّ ٱلْعَٰلَمِين٣. ٱلرَّحْمَٰنِ ٱلرَّحِيمِ٤. مَٰلِكِ يَوْمِ ٱلدِّينِ٥. إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ٦. ٱهْدِنَا ٱلصِّرَٰطَ ٱلْمُسْتَقِيمَ٧. صِرَٰطَ ٱلَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ ٱلْمَغْضُوبِ عَلَيْهِمْ وَلَا ٱلضَّآلِّينَ",
+          phonetic:
+            "Bismil laahi-r raĥmaani-r raĥiimi Al-Ĥamdu Lillāhi Rabbi Al-`Ālamīna, Ar-Raĥmāni Ar-Raĥīmi, Maliki Yawmid Dīni, Īyyaaka Nahbudu Wa Īyyaaka Nastahiinu, Ihdinas siraatal mustaqīma, siraatal laziina anhamta halayhim ghayril maghduubi halayhim Wa Lad- daal’liina. (1)",
+        },
+        {
+          arabe: " Lakhad Jaakoum 60",
+          phonetic:
+            "Lakhad jaa’akum rasuulun min anfusikum haziizun halayhi maa hanittum hariisune halaykum bil-muuminiina ra'uufun rahiimun, fa'in tawallaw faqul hasbiyal laahu laa ilaaha illaa huwa halayhi tawakkaltu wa huwa rabbul harshil haziimi.(60)",
+        },
+        {
+          arabe:
+            "ََأََعََوَذَََبَاَللهَََمََنَََاَلَشََيََطََاَنََاََلَرََ َجَيَمََ",
+          phonetic:
+            "Ahuuzu biallahi minash shayţānir rajiimi In Kaanat illaa sayhatane waahidatane fa izaa hum khaamiduuna. (1)",
+        },
+        {
+          arabe: "بِسْمِ اللّهِ الرَّحْمَنِ الرَّحِيمِ ",
+          phonetic:
+            "Bismil laahi-r raĥmaani-r raĥiimi Al-laziina kafaruu wa sad’duu han sabiilil laahi adalla ahmaalahum, wal laziina aamanuu wa hamilus saalihaati, wa aamanuu bimaa nuzzila halaa muhammadine, wa huwal haqqu min rabbihim kaffara hanhum sayyi'aatihim, wa aslaha baalahum. (1)",
+        },
+        {
+          arabe: "أعوذَباللهَمنَالشيطانَالرجيم",
+          phonetic:
+            "Ahuuzu biallahi minash shayţānir rajiimi,Sayuhzamul jamhu, wa yuwal’luunad dubura. (3)",
+        },
+        {
+          arabe: "أعوذَباللهَمنَالشيطانَالرجيم",
+          phonetic:
+            "Ahuuzu biallahi minash shayţānir rajiimi Falaa uqsimu bi mawaa qihine nujuumi, wa innahu laqasamune law tahlamuuna haziimune. (1)",
+        },
+        {
+          arabe: "أعوذَباللهَمنَالشيطانَالرجيم",
+          phonetic:
+            "Ahuuzu biallahi minash shayţānir rajiimi Fa aatāhumul laahu min haysou lam yahtasibuu(1)",
+        },
+        {
+          arabe: "",
+          phonetic:
+            "Bismil laahi-r raĥmaani-r raĥiimi Alam tara kayfa fahala rabbuka bi ashaabil fiili, alam yajhal kaydahum fii tadliiline, wa arsala halayhim tayrane abaabiila, tarmiihim bihijaaratine min sijiiline, fajaha lahum ka hasfine maa’kuuline. (1)",
+        },
+        {
+          arabe: " surat kaafirun",
+          phonetic:
+            "Bismil laahi-r raĥmaani-r raĥiimi Qul yaa ayyuhal kaafiruuna, laa ahbudu maa tahbuduuna, walaa antum haabiduuna maa ahbudu, walaa anaa haabidune maa habad’tum, walaa laa antum haabiduna maa ahbudu, lakum diinukum wa liya diini. (1)",
+        },
+
+        {
+          arabe: "",
+          phonetic:
+            "Subhaana rabbika, rabbil hizzati, hammaa yasifuuna, wa salaamun halal mursaliina, wal hamdu lil’laahi rabbil haalamiina. (1)",
+        },
+      ];
+
+      let currentVerse = 0;
+
+      function showVerse() {
+        document.getElementById("arabe").textContent = wird[currentVerse].arabe;
+        document.getElementById("phonetic").textContent =
+          wird[currentVerse].phonetic;
+      }
+
+      function nextVerse() {
+        if (currentVerse < wird.length - 1) {
+          currentVerse++;
+          showVerse();
+        }
+      }
+
+      function prevVerse() {
+        if (currentVerse > 0) {
+          currentVerse--;
+          showVerse();
+        }
+      }
+
+      // Affiche le premier verset au chargement
+      showVerse();
+   
---
+++ scriptSalawat.js
@@ -0,0 +1,50 @@
+let count = 0;
+
+const compteur = document.getElementById("compteur");
+const message = document.getElementById("message");
+const btnPlus = document.getElementById("btnPlus");
+const btnReset = document.getElementById("btnReset");
+
+function animerCompteur() {
+  compteur.classList.add("animate"); // ajoute la classe → animation démarre
+
+  setTimeout(() => {
+    compteur.classList.remove("animate"); // retire la classe → permet de relancer l’animation
+  }, 300); // 0.3s = durée de l’animation
+}
+function condition(){
+  if (count > 1947) {
+    count = 0;
+    compteur.style.color = "red";
+    message.textContent = "Bravo! Salawat complété,Compteur remis a Zero!";
+  } else if (count < 100) {
+    compteur.style.color = "white";
+    message.textContent = "";
+  } else if (count >= 100 || count > 500) {
+    compteur.style.color = "blue";
+    message.textContent = "";
+  } else if (count >= 500 && count > 1000) {
+    compteur.style.color = "green";
+    message.textContent = "";
+  } else if (count >= 1000 && count > 1500) {
+    compteur.style.color = "yellow";
+    message.textContent = "";
+  } else if (count >= 1500 && count > 1946) {
+    compteur.style.color = "orange";
+    message.textContent = "";
+  }
+}
+function incrementer() {
+  count++;
+  condition()
+  compteur.textContent = String(count);
+  animerCompteur();
+}
+
+function reset() {
+  count = 0;
+  compteur.textContent = String(count);
+}
+
+btnPlus.addEventListener("click", incrementer);
+btnReset.addEventListener("click", reset);
